24f04625e0b12de236da40b8dd3284c876980917
Valentin <93974733+valentindush@users.noreply.github.com>
2022-08-04 23:04:17 +0200

    nav bar done !

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,27 @@ import { NavBar } from './components/Navbar';
 
 function App() {
 
+  let options = {
+    sectionClassName:     'section',
+    anchors:              ['about', 'projects', 'skills', 'contact'],
+    scrollBar:            false,
+    navigation:           true,
+    verticalAlign:        false,
+    sectionPaddingTop:    '50px',
+    sectionPaddingBottom: '50px',
+    arrowNavigation:      true
+  };
+
 
 
   return (
     <div className="App bg-[#092027] h-screen w-screen">
 
-      <Blurred pos={["20%","100px"]} />
+      <Blurred className={"top-[30%] left-12"}/>
+      <Blurred className={"top-[40%] right-8"}/>
+
+      
+
 
       <Header>
         <NavBar/>
--- src/components/blured.jsx
+++ src/components/blured.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-export const Blurred = ({pos}) => {
-    const left = pos[0]
-    const top = pos[1]
-    console.log(left, top)
+export const Blurred = ({className}) => {
+
   return (
-    <div className={`w-[100px] h-[100px] bg-cyan-500 blur-3xl fixed top-[${top}] left-[${left}]`}>
+    <div className={`w-[100px] h-[100px] bg-cyan-500 blur-3xl fixed ${className}`}>
 
     </div>
   )
