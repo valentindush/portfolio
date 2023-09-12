b9a6f9c7cafe9bbda7edc8bd10a10594cf35f0ba
valentindush <codesvalentin@gmail.com>
2023-09-12 19:18:36 +0200

    upd: glass effect !!

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,59 @@
+import bgImage from './assets/R.jpeg'
 
-import {SectionsContainer, Section, Header, Footer} from 'react-fullpage';
-import { Blurred } from './components/blured';
-import { Footer_A } from './components/Footer';
-import { NavBar } from './components/Navbar';
-import {About} from './components/sections/About';
-import { Contact } from './components/sections/Contact';
-import { Projects } from './components/sections/Projects';
-import {Skills} from './components/sections/Skills'
-import Menu from './components/menu';
 function App() {
 
-  let options = {
-    activeClass:     'navActive',
-    anchors:              ['about', 'skills', 'projects', 'contact'],
-    scrollBar:            false,
-    navigation:           true,
-    verticalAlign:        false,
-    sectionPaddingTop:    '50px',
-    sectionPaddingBottom: '50px',
-    arrowNavigation:      true
-  };
-
-
-
   return (
-    <div className="App bg-white h-screen w-screen overflow-x-hidden relative px-24 md:px-6">
-      <Menu />
-      <About />  
+    <div className="overflow-y-hidden w-screen h-screen py-24">
+       <img className='w-screen brightness-75 z-0 absolute top-0 left-0 h-screen object-cover blur-sm' src="/img/R.jpeg" alt="" />
+        <div className="bg-white/20 max-w-2xl h-[calc(100vh-12rem)] p-2 relative z-20 mx-auto rounded-xl border-2 border-white/20 backdrop-blur-sm">
+          <div className="w-full h-full overflow-y-scroll p-4">
+            <div className="text-white">
+              <h1 className="text-4xl font-bold text-center">Hello World</h1>
+              <div className="mt-2">
+              Hello, I'm [Your Name]
+
+Software Engineer | Machine Learning Enthusiast | AI Aficionado
+
+🌟 Passionate about transforming ideas into elegant code and solving complex problems with technology.
+
+As a software engineer, I'm on a mission to bridge the gap between human imagination and digital reality. With a strong foundation in software development and a fervent interest in machine learning and artificial intelligence, I've embarked on a journey to shape the future of technology.
+
+🚀 What I Bring to the Table:
+
+✨ Expertise in Software Engineering: With a solid background in software development, I excel in crafting clean, efficient, and scalable code. I've honed my skills in a variety of programming languages, frameworks, and technologies.
+
+🤖 Machine Learning Maven: Machine learning is not just a buzzword for me; it's a passion. I've delved into the depths of neural networks, deep learning, natural language processing, and computer vision. Whether it's predicting trends, optimizing processes, or making predictions, I'm up for the challenge.
+
+🧠 AI Enthusiast: Artificial intelligence is more than just algorithms; it's the key to unlocking the potential of our digital world. I'm dedicated to exploring the frontiers of AI, from developing smart chatbots to creating intelligent recommendation systems.
+
+🌐 Full-Stack Explorer: I'm not just confined to one layer of the tech stack. From front-end design that captivates users to back-end logic that powers applications, I'm well-versed in the entire development cycle.
+
+🛠️ Problem-Solver: My engineering mindset thrives on solving real-world problems. Whether it's optimizing business processes, enhancing user experiences, or tackling complex data analysis, I relish the opportunity to find innovative solutions.
+
+🤝 Collaborative Team Player: I believe in the power of collaboration. Working closely with cross-functional teams, I've learned that the best solutions often arise from diverse perspectives and collective creativity.
+
+📚 Lifelong Learner: The tech world never stops evolving, and neither do I. I'm committed to continuous learning, staying updated with the latest trends, and constantly improving my skills.
+
+🎯 My Portfolio Highlights:
+
+👨‍💻 [Project Name 1]: [Brief description and link]
+
+[Key achievement or technology used]
+🤖 [Project Name 2]: [Brief description and link]
+
+[Key achievement or technology used]
+🚀 [Project Name 3]: [Brief description and link]
+
+[Key achievement or technology used]
+📝 Let's Connect!
+
+I'm always excited to connect with fellow tech enthusiasts, potential collaborators, or anyone who shares a passion for the limitless possibilities of programming, machine learning, and AI. Feel free to reach out to me at [Your Email] or connect with me on [LinkedIn/GitHub/other social media] to start a conversation.
+
+Let's build the future together, one line of code at a time!
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
